7f8f1ade429e062c691dfafe2fc1255b9c5819ab
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2023-01-20 17:23:44 -0300

    build

---
+++ README.md
@@ -0,0 +1,49 @@
+# Natura Design System - Styles
+
+![Travis (.org)](https://img.shields.io/travis/natura-cosmeticos/natds-js.svg)
+[![Known Vulnerabilities](https://snyk.io/test/github/natura-cosmeticos/natds-js/badge.svg?targetFile=package.json)](https://snyk.io/test/github/natura-cosmeticos/natds-js?targetFile=package.json)
+
+## Before you start...
+
+We encourage you to use the repository
+[@naturacosmeticos/natds-web](https://github.com/natura-cosmeticos/natds-js/tree/master/packages/web) directly.
+
+However, if you are working on a project that does not use React, so you can adopt styles in your web application or website.
+
+By the way, [@naturacosmeticos/natds-web](https://github.com/natura-cosmeticos/natds-js/tree/master/packages/web) uses this package as a dependency.
+
+## How to install
+
+```shell script
+# npm
+npm install --save @naturacosmeticos/natds-styles
+
+# yarn
+yarn add @naturacosmeticos/natds-styles
+```
+
+### How to use
+
+```javascript
+// ES6 or above
+import {tokens} from "@naturacosmeticos/natds-styles"
+
+// ES5 or below
+const {tokens} = require("@naturacosmeticos/natds-styles")
+```
+
+Check our [Design System Storybook](https://storybook-web.natura.com.br/) for all available design tokens.
+
+## How to contribute
+
+To contribute, please check out our contributing file [`CONTRIBUTING`](./CONTRIBUTING.md)
+
+---
+
+## Check also
+
+For more details on how we use these styles or implementation details, please refer to the following links:
+
+- [@naturacosmeticos/natds-web project](https://github.com/natura-cosmeticos/natds-js/tree/master/packages/web)
+- [@naturacosmeticos/natds-web provider](https://github.com/natura-cosmeticos/natds-js/tree/master/packages/web/src/Provider/index.tsx)
+
--- dist/tokens/colors/themeBound/index.d.ts
+++ dist/tokens/colors/themeBound/index.d.ts
@@ -1,76 +1,66 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.colorTokens = void 0;
-/* eslint-disable import/prefer-default-export */
-var natura_1 = require("./natura");
-var avon_1 = require("./avon");
-var theBodyShop_1 = require("./theBodyShop");
-var aesop_1 = require("./aesop");
-var biome_1 = require("./biome");
-var elo_1 = require("./elo");
 /**
  * @deprecated theme-bound `colorTokens` are deprecated since v0.20 and will be removed at v1.0.
  * Please use `theme.${brand}.${mode}.color` from `natds-themes`.
  */
-exports.colorTokens = {
+export declare const colorTokens: {
     /**
      * @deprecated `avonDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.avon.dark.color` from `natds-themes`.
      */
-    avonDarkColorTokens: avon_1.avonDarkColorTokens,
+    avonDarkColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `avonLightColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.avon.light.color` from `natds-themes`.
      */
-    avonLightColorTokens: avon_1.avonLightColorTokens,
+    avonLightColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `naturaDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.natura.dark.color` from `natds-themes`.
      */
-    naturaDarkColorTokens: natura_1.naturaDarkColorTokens,
+    naturaDarkColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `naturaLightColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.natura.light.color` from `natds-themes`.
      */
-    naturaLightColorTokens: natura_1.naturaLightColorTokens,
+    naturaLightColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `theBodyShopDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.theBodyShop.dark.color` from `natds-themes`.
      */
-    theBodyShopDarkColorTokens: theBodyShop_1.theBodyShopDarkColorTokens,
+    theBodyShopDarkColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `theBodyShopLightColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.theBodyShop.light.colors` from `natds-themes`.
      */
-    theBodyShopLightColorTokens: theBodyShop_1.theBodyShopLightColorTokens,
+    theBodyShopLightColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `aesopDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.aesop.dark.color` from `natds-themes`.
      */
-    aesopDarkColorTokens: aesop_1.aesopDarkColorTokens,
+    aesopDarkColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `aesopLightColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.aesop.light.colors` from `natds-themes`.
      */
-    aesopLightColorTokens: aesop_1.aesopLightColorTokens,
+    aesopLightColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
-     * @deprecated `aesopDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
-     * Please use `theme.aesop.dark.color` from `natds-themes`.
+     * @deprecated `biomeDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
+     * Please use `theme.biome.dark.color` from `natds-themes`.
      */
-    biomeDarkColorTokens: biome_1.biomeDarkColorTokens,
+    biomeDarkColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `biomeLightColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.biome.light.colors` from `natds-themes`.
      */
-    biomeLightColorTokens: biome_1.biomeLightColorTokens,
+    biomeLightColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `eloDarkColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.elo.dark.color` from `natds-themes`.
      */
-    eloDarkColorTokens: elo_1.eloDarkColorTokens,
+    eloDarkColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
     /**
      * @deprecated `eloLightColorTokens` are deprecated since v0.20 and will be removed at v1.0.
      * Please use `theme.elo.light.colors` from `natds-themes`.
      */
-    eloLightColorTokens: elo_1.eloLightColorTokens
+    eloLightColorTokens: import("./IColorThemeTokens").IColorThemeTokens;
 };
-//# sourceMappingURL=index.js.map
